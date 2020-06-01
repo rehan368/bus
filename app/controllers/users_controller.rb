4011219b18667ca2c416bf9dd67d4925c7824cdf
Rehan Raza <rhn.raza121@gmail.com>
2020-06-01 12:33:50 +0530

    add user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    def create
+        @user=User.new(params.require(:user).permit(:username, :email, :password))
+        if @user.save
+           #session[:user_id] = @user.id
+            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :reservations, except: [:edit, :show]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
