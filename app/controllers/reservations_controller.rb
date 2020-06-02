24439f13172a9ef9c10f8129b54566d5d5e93ca6
Rehan Raza <rhn.raza121@gmail.com>
2020-06-02 22:23:30 +0530

    add restriction

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,13 @@
 class ReservationsController < ApplicationController
+    before_action :require_user
+    before_action :require_same_user, only: [:destroy]
     def new
         @reservation = Reservation.new
     end
 
     def create
         @reservation = Reservation.new(params.require(:reservation).permit(:name,:from,:to,:doj,:time))
+        @reservation.user=current_user
         if params[:reservation][:from].eql?params[:reservation][:to]
             flash.now[:alert] = "Source and destination can't be same"
             render 'new'
@@ -26,5 +29,12 @@ class ReservationsController < ApplicationController
         @reservation = Reservation.find(params[:id])
         @reservation.destroy
         redirect_to reservations_path
+    end
+
+    def require_same_user
+        if current_user!=@reservation.user
+            flash[:alert]="You can only edit or delete your own article"
+            redirect_to reservations_path
+        end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :destroy]
+    before_action :require_user
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def new
         @user = User.new
     end
@@ -6,8 +9,8 @@ class UsersController < ApplicationController
     def create
         @user=User.new(params.require(:user).permit(:username, :email, :password))
         if @user.save
-           #session[:user_id] = @user.id
-            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+            session[:user_id] = @user.id
+            flash[:notice] = "Welcome to the Online Bus Booking #{@user.username}, you have successfully signed up"
             redirect_to new_reservation_path
         else
             render 'new'
@@ -15,11 +18,10 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user=User.find(params[:id])
+        
     end
 
     def update
-        @user=User.find(params[:id])
         if @user.update(params.require(:user).permit(:username, :email, :password))
             flash[:notice] = "Your account information was successfully updated"
             redirect_to new_reservation_path
@@ -29,10 +31,19 @@ class UsersController < ApplicationController
     end
 
     def destroy 
-        @user=User.find(params[:id])
         @user.destroy
-        #session[:user_id]=nil if @user==current_user
-        flash[:notice] = "Account and all associated articles successfully deleted"
+        session[:user_id]=nil 
+        flash[:notice] = "Account and all associated reservations successfully deleted"
         redirect_to root_path
+    end
+    private
+    def set_user
+        @user=User.find(params[:id])
+    end
+    def require_same_user
+        if current_user!=@user
+            flash[:alert]="You can only edit or delete your own account"
+            redirect_to root_path
+        end
     end
 end
